3bf4bc9a6a78aad52dfb51af038fc3cb65239d58
PedroAlcazarGuerrero <159447603+PedroAlcazarGuerrero@users.noreply.github.com>
2026-05-05 14:17:00 +0200

    choose_region implementada

--- modules/analysis.py
+++ modules/analysis.py
@@ -8,8 +8,23 @@ def hello():
 def loadcsv(ifile):
   return pd.read_csv(ifile,header=None,sep=";").replace(",",".",regex=True).to_numpy().astype("float")
 
-def pintar_imagen(filename,vmin=None,vmax=None,colormap="coolwarm",colorbar=False):
+def pintar_imagen(filename,vmin=None,vmax=None,colormap="coolwarm",colorbar=False,savepath=""):
   plt.imshow(loadcsv(filename),cmap=colormap,vmin=vmin,vmax=vmax)
   if colorbar:
     plt.colorbar()
+  if savepath != "":
+    plt.savefig(savepath)
+
+
+def choose_region(filename,origin,shape,vmin=None,vmax=None,colormap="coolwarm",savepath=""):
+  datos=loadcsv(filename)
+  pintar_imagen(filename,vmin,vmax,colormap)
+  region=np.zeros_like(datos)
+  end=np.array(origin)+np.array(shape)
+  region[origin[0]:end[0],origin[1]:end[1]]=1
+  region=region.astype(bool)
+  plt.imshow(region,alpha=.2,cmap="binary")
+  if savepath != "":
+    plt.savefig(savepath)
+  return region
     
