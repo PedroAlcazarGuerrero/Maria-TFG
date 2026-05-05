477ddc24c1f496b3e188052faeb04ff96839e160
PedroAlcazarGuerrero <159447603+PedroAlcazarGuerrero@users.noreply.github.com>
2026-05-05 13:20:34 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,6 +4,9 @@ import pylab as plt
 def hello():
   print("hello")
 
+def loadcsv(ifile):
+  return pd.read_csv(ifile,header=None,sep=";").replace(",",".",regex=True).to_numpy().astype("float")
+
 def pintar_imagen(filename,vmin=None,vmax=None,colormap="coolwarm"):
   plt.imshow(loadcsv(filename),cmap=colormap,vmin=vmin,vmax=vmax)
     
