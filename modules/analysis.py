0046d483de9f64701ca71784d830cc224bad8d8f
PedroAlcazarGuerrero <159447603+PedroAlcazarGuerrero@users.noreply.github.com>
2026-07-06 12:54:17 +0200

    posibilidad de db

--- modules/analysis.py
+++ modules/analysis.py
@@ -30,22 +30,22 @@ def choose_region(filename,origin,shape,vmin=None,vmax=None,colormap="coolwarm",
 
 
 def extract_region_from_db(data,name,db):
-  this_entry=db.loc[name]
+  this_entry=db.loc[name.split("/")[-1]]
   region=np.zeros_like(data,dtype=float)
   x_min=int(this_entry["x_min"])
   x_max=int(this_entry["x_max"])
   y_min=int(this_entry["y_min"])
   y_max=int(this_entry["y_max"])
   region[x_min:x_max,y_min:y_max]=1
-  return region
+  return region.astype(bool)
 
-def plot_T_evolution(prefix,T_list,region,title,tagpos=[80,22],color="black",fitting=False,color_secundario="tab:blue",savepath=None,emisivity=1,default_db="MARIA-TFG/datos/Base_de_datos_regiones.csv"):
+def plot_T_evolution(prefix,T_list,region,title,tagpos=[80,22],color="black",fitting=False,color_secundario="tab:blue",savepath=None,emisivity=1,default_db="/content/Maria-TFG/datos/Estufa/Base_de_datos_regiones.csv"):
   Tavg=[]
   Tmax=[]
   dbsing=False
   if region is None:
     dbsing=True
-    db=pd.loadcsv(default_db,sep=";",index_col="name")
+    db=pd.read_csv(default_db,sep=";",index_col="name")
   for T in T_list:
     data=loadcsv(prefix+str(T)+".csv")*emisivity
     if region is None:
